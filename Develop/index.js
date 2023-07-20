45452987a0628f878c17cfe6fdd8fdfda5513c92
Rockojoe2 <lee.d.joey@gmail.com>
2023-07-20 13:52:32 -0500

    Got user prompt and answer to come, but can't get multiple questions to appear

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,13 @@
 // TODO: Include packages needed for this application
 
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -13,10 +21,18 @@ const questions = [
 
 const answers = [];
 
-for(i = 0; i < questions.length; i++)
-{
-    let input = prompt(questions[i].question);
-    answers.push(input);
+function answerQuestions() {
+
+    for(i = 0; i < questions.length; i++)
+    {
+        rl.question(questions[i].question, function(answer){
+            console.log("This is your answer" + answer);
+            answers.push(answer)
+            console.log(answers);
+            rl.close();
+        });
+    
+    }
 }
 
 // TODO: Create a function to write README file
@@ -27,4 +43,8 @@ function init() {}
 
 // Function call to initialize app
 init();
+
 console.log("This is how many questions we have: " + questions.length);
+console.log("This is the question to question 1: " + questions[0].question);
+answerQuestions();
+
